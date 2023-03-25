340a6261c82ece47a2e0d5931f3ce8d3394a8e88
Paulo Capelo <paulodcsc@gmail.com>
2023-03-25 01:49:31 -0300

    Add new global style for Next app
    This commit adds a new global style for the Next app, which will be applied across all pages and components.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link";
 
+function Title({ children }) {
+  return (
+    <>
+      <h1>{children}</h1>
+      <style jsx>{`
+        h1 {
+          color: white;
+        }
+      `}</style>
+    </>
+  );
+}
+
 export default function HomePage() {
   return (
     <>
-      <h1>PÁGINA INICIAL</h1>
+      <Title>PÁGINA INICIAL</Title>
       <Link href="/secondary">Outra página</Link>
     </>
   );
---
+++ src/theme/GlobalStyle.tsx
@@ -0,0 +1,10 @@
+export default function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      body {
+        font-family: sans-serif;
+        background: #2b2b2b;
+      }
+    `}</style>
+  );
+}
